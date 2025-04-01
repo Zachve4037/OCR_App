9b2b7299b62a8376e06387ce07838baee2bfd162
Zachve4037 <matej.zachveja@gmail.com>
2025-04-01 20:42:12 +0200

    zoom of images finished

--- src/Loader.py
+++ src/Loader.py
@@ -1,14 +1,17 @@
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QFileDialog, QGraphicsScene
 
 class Loader:
-    def __init__(self, label):
-        self.label = label
+    def __init__(self, view):
+        self.view = view
+        self.scene = QGraphicsScene()
+        self.view.setScene(self.scene)
 
     def load_image(self):
         options = QFileDialog.Options()
         file_name, _ = QFileDialog.getOpenFileName(None, "Open Image File", "", "Images (*.png *.xpm *.jpg *.bmp *.gif)", options=options)
         if file_name:
             pixmap = QPixmap(file_name)
-            self.label.setPixmap(pixmap)
-            self.label.setScaledContents(True)
+            self.scene.clear()
+            self.scene.addPixmap(pixmap)
+            self.view.fitInView(self.scene.itemsBoundingRect(), mode=1)
